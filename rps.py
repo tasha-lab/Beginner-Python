f2bb8188e25553a657caaea7383e52b7ab6950a1
Halima <natashawanjiku31@gmail.com>
2026-05-02 00:03:03 +0300

    add

--- rps.py
+++ rps.py
@@ -3,52 +3,78 @@ import sys
 import random
 from enum import Enum
 
-class RPS(Enum):
-    ROCK = 1
-    PAPER = 2
-    SCISSORS = 3
+def play_rps():
+    game_count=0
+    player_wins=0
+    comp_wins = 0
+
 # print(RPS(2))
 # print(RPS.ROCK)#RPS.ROCK
 # print(RPS['ROCK'])#RPS.ROCK
 # print(RPS.ROCK.value)#1
 # sys.exit()
-def rps():
-    playerChoice=input('\nEnter...\n1 for rock\n2 for paper or \n3 for scissors:\n\n')
-    if playerChoice not in ["1","2","3"]:
-        print('You must chose a number between 1 and 3')
-        return rps()
-    player = int(playerChoice)
+    def rps():
+        nonlocal player_wins
+        nonlocal comp_wins
 
-    compChoice = random.choice("123")
-    comp = int(compChoice)
+        class RPS(Enum):
+            ROCK = 1
+            PAPER = 2
+            SCISSORS = 3
 
-    
-    print('\nyou chose '+str(RPS(player)).replace('RPS.','')+'.')
-    print('computer chose '+ str(RPS(comp)).replace('RPS.','') +'.\n')
-    
+        playerChoice=input('\nEnter...\n1 for rock\n2 for paper or \n3 for scissors:\n\n')
+        if playerChoice not in ["1","2","3"]:
+            print('You must chose a number between 1 and 3')
+            return rps()
+        player = int(playerChoice)
 
-    if player == 1 and comp ==3:
-        print('🥳 You win!')
-    elif player == 2 and comp ==1:
-        print('🥳 You win!')
-    elif player == 3 and comp ==2:
-        print('🥳 You win!')
-    elif player == comp:
-        print('😲 Its a tie!')
-    else:
-        print('🐍 You lose!')
-    print('\n play again?')
-    while True:
-        playAgain =input(' \nY for yes\nQ for quit\n')
-        if playAgain.lower() not in ['y','q']:
-            continue
+        compChoice = random.choice("123")
+        comp = int(compChoice)
+
+        
+        print('\nyou chose '+str(RPS(player)).replace('RPS.','')+'.')
+        print('computer chose '+ str(RPS(comp)).replace('RPS.','') +'.\n')
+        
+        def decide_winner(player,comp):
+            nonlocal player_wins
+            nonlocal comp_wins
+            if player == 1 and comp ==3:
+                player_wins+=1
+                return '🥳 You win!'
+            elif player == 2 and comp ==1:
+                player_wins+=1
+                return '🥳 You win!'
+            elif player == 3 and comp ==2:
+                player_wins+=1
+                return '🥳 You win!'
+            elif player == comp:
+                return '😲 Its a tie!'
+            else:
+                comp_wins+=1
+                return '🐍 You lose!'
+        game_result = decide_winner(player,comp)
+        print(game_result)
+
+        nonlocal game_count 
+        game_count+=1
+
+        print('\nGame count:'+str(game_count))
+        print('\nPlayer wins:'+str(player_wins))
+        print('\ncomputer wins:'+str(comp_wins))
+        print('\nplay again?')
+        while True:
+            playAgain =input(' \nY for yes\nQ for quit\n')
+            if playAgain.lower() not in ['y','q']:
+                continue
+            else:
+                break
+
+        
+        if playAgain.lower()=='y':
+            return rps()
         else:
-            break
-
-    
-    if playAgain.lower()=='y':
-        return rps()
-    else:
-        print('\n🥳🥳\nThank you for playing')
-        sys.exit("Bye 👋")
-rps()
+            print('\n🥳🥳\nThank you for playing')
+            sys.exit("Bye 👋")
+    return rps
+play =play_rps()
+play()
--- scope.py
+++ scope.py
@@ -1,5 +1,6 @@
 #global scope. available to everything in the file.defined outside a function
 name = "Tasha"
+count = 1
 
 # def greeting():
 #     print(name)
@@ -7,12 +8,27 @@ name = "Tasha"
 #     print(color)
 # greeting()
 # print(color)
-def greeting(firstname):
-    print(firstname)
-    color='blue' #local scope
-    print(color)
-greeting('John')
+# def greeting(firstname):
+#     print(firstname)
+#     color='blue' #local scope
+#     print(color)
+# greeting('John')
 
+# def anotherFunction():
+#     greeting('Dave')
+# anotherFunction()
 def anotherFunction():
-    greeting('Dave')
+    color='blue'
+    #referencing a global scope while a similar local scope is defined
+    global count
+    count+=2
+    print(count)
+    def greeting(name):
+        #reading a value from a parent function then reassigning it
+        nonlocal color
+        color = 'red'
+        print(name)
+        print(color)
+    
+    greeting('John')
 anotherFunction()
