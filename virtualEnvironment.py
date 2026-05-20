b0e56f4a8a3bd4031138f6672d0237c3f15089ee
Halima <natashawanjiku31@gmail.com>
2026-05-20 12:27:53 +0300

    virtual environment

---
+++ virtualEnvironment.py
@@ -0,0 +1,7 @@
+#if pip install doesnt work, use py -m pip install
+#pip installls modules globally
+#py -m venv- cretae a virtual environment
+#source .venv/Scripts/activate - activate the virtual environment
+#deactivate- deactivate
+#pypi.org-python packages
+#pip freeze > requirements.txt- dependencies
---
+++ weather.py
@@ -0,0 +1,22 @@
+import requests
+from dotenv import load_dotenv
+import os
+from pprint import pprint
+
+load_dotenv()
+
+def get_current_weather():
+    print("\n***Get Current Weather Conditions***\n")
+    city =input("\nPLease enter a city name:\n")
+
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+    #print(request_url)
+    weather_data = requests.get(request_url).json()
+    #pprint(weather_data) use pprint to be able to read the json 
+    print(f"\nCurrent weather for {weather_data["name"]}")
+    print(f"\nThe temp is {weather_data["main"]["temp"]}")
+    print(f"\nFeels like {weather_data["main"]["feels_like"]} and {weather_data["weather"][0]["description"]}")
+
+if __name__ == "__main__":
+    get_current_weather()
+
